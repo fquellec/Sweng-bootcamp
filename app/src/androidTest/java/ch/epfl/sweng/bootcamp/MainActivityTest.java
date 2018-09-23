97ad31fa8b2c840987cbda1f60d5738f801fabac
francoisquellec <francois.quellec@heig-vd.ch>
2018-09-23 22:25:37 +0200

    add tests for the app

---
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -0,0 +1,24 @@
+package ch.epfl.sweng.bootcamp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testCanGreetUsers() {
+        Espresso.onView(ViewMatchers.withId(R.id.mainName)).perform(ViewActions.typeText("from my unit test")).perform(ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.mainButton)).perform(ViewActions.click());
+        // onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+    }
+}
